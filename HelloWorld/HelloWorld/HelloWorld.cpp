5c6d098ca7f273dbea23045031698ca148984dc4
Dj Brynz <Dj Brynz@DESKTOP-CAJ2PDG>
2020-04-08 19:14:25 -0500

    Operaciones

--- HelloWorld/HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -2,12 +2,63 @@
 //
 
 #include <iostream>
-
+/* Este sera un programa que imprima un Hello World*/
 int main()
 {
+    // Este es un comentario de una linea /*
+    /* Operador de Salida >> */
+    /* Salto de linea \n */
+    /* Tabulacion \t */
+    /* Todo empieza en la funcion main */
+
     std::cout << "Hello World!\n";
     std::cout << "Bienvenidos!\n";
-    std::cout << "El mejor curso!\n";
+    std::cout << "\tAl curso Ciencia de la Computacion I !\n";
+    std::cout << "\t\tParticipar es tu mejor opcion!\n";
+    
+
+    /* SUMA */
+    //Debo declarar las variables
+    int x;
+    int y;
+    int z;
+    int q;
+    int w;
+    int a;
+    int b;
+    int suma;
+    int resta;
+    int multi;
+
+
+    //Inicializar las variables
+    x = 10;
+    y = 15;
+    z = x + y;
+
+    std::cout << z << std::endl;
+
+    
+     //Inicializar las variables 2
+    std::cout << "Ingrese un numero por favor : ";
+    std::cin >> q;
+    std::cout << "Ingrese otro numero por favor : ";
+    std::cin >> w;
+    a = q + w;
+    std::cout << "El resultado de la suma es : ";
+    std::cout << a << std::endl;
+
+    resta = q - w;
+    multi = q * w;
+
+    std::cout << "El resultado de la resta es : " << resta << std::endl;
+    std::cout << "El resultado de la multiplicacion es : " << multi << std::endl;
+
+    
+ 
+
+
+    return 0;
 }
 
 // Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
